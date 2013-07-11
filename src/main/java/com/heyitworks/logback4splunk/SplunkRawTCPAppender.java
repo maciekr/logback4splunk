1afe9f3df8b92dd2b7ee9078e65f6e65af209b52
maciekr <maciek_r@ymail.com>
2013-07-11 19:54:53 +0200

    - clean up this messy code

--- src/main/java/com/heyitworks/logback4splunk/SplunkRawTCPAppender.java
+++ src/main/java/com/heyitworks/logback4splunk/SplunkRawTCPAppender.java
@@ -12,60 +12,35 @@ import ch.qos.logback.core.Layout;
  */
 public class SplunkRawTCPAppender extends AppenderBase<ILoggingEvent> {
 
-	// connection settings
-	private String host = "";
-	private int port = 5150;
+	private String host;
+	private int port;
 
-	//queuing settings
-	private String maxQueueSize; 
-	private boolean dropEventsOnQueueFull;
-	
 	private SplunkRawTCPInput sri;
-
 	private Layout<ILoggingEvent> layout;
 
-	/**
-	 * Constructor
-	 */
-	public SplunkRawTCPAppender() {
-	}
-
-	/**
-	 * Log the message
-	 */
 	@Override
 	protected void append(ILoggingEvent event) {
         if (sri != null) {
-
 			String formatted = layout.doLayout(event);
-
-			sri.streamEvent(formatted);
-
+            sri.streamEvent(formatted);
 		}
 	}
 
-	/**
-	 * Initialisation logic
-	 */
 	@Override
 	public void start() {
-
-		if (this.layout == null) {
+        if (this.layout == null) {
 			addError("No layout set for the appender named [" + name + "].");
 			return;
 		}
-
 		if (sri == null) {
 			try {
 				sri = new SplunkRawTCPInput(host, port);
-				sri.setMaxQueueSize(maxQueueSize);
-				sri.setDropEventsOnQueueFull(dropEventsOnQueueFull);
 			} catch (Exception e) {
 				addError("Couldn't establish Raw TCP connection for SplunkRawTCPAppender named \""
 						+ this.name + "\".");
 			}
 		}
-		super.start();
+        super.start();
 	}
 
 	/**
@@ -101,22 +76,6 @@ public class SplunkRawTCPAppender extends AppenderBase<ILoggingEvent> {
 		this.port = port;
 	}
 
-	public String getMaxQueueSize() {
-		return maxQueueSize;
-	}
-
-	public void setMaxQueueSize(String maxQueueSize) {
-		this.maxQueueSize = maxQueueSize;
-	}
-
-	public boolean isDropEventsOnQueueFull() {
-		return dropEventsOnQueueFull;
-	}
-
-	public void setDropEventsOnQueueFull(boolean dropEventsOnQueueFull) {
-		this.dropEventsOnQueueFull = dropEventsOnQueueFull;
-	}
-	
 	public Layout<ILoggingEvent> getLayout() {
 		return layout;
 	}
--- src/main/java/com/heyitworks/logback4splunk/SplunkRawTCPInput.java
+++ src/main/java/com/heyitworks/logback4splunk/SplunkRawTCPInput.java
@@ -13,19 +13,20 @@ import java.util.concurrent.*;
  * @author Damien Dallimore damien@dtdsoftware.com
  */
 
-public class SplunkRawTCPInput extends SplunkInput {
+public class SplunkRawTCPInput {
 
-    // connection props
-    private String host = "";
+    private String host;
     private int port;
 
-    // streaming objects
-    private Socket streamSocket = null;
-    private OutputStream ostream;
-    private Writer writerOut = null;
+    private static int BUFFER_COUNT = 1000;
 
-    //maciekr - need to have something like that to effectively protect OutputStreamWriter from concurrent modification as there is no protection of any sort in this code
-    private static final ExecutorService executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(1000), new ThreadFactory() {
+    // streaming objects
+    private Socket streamSocket;
+    private Writer writerOut;
+
+    //maciekr - need to have something like that to effectively protect OutputStreamWriter from concurrent modification as there was no protection of any sort in this code
+    private static final ExecutorService executor =
+            new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(BUFFER_COUNT), new ThreadFactory() {
         @Override
         public Thread newThread(Runnable runnable) {
             return new Thread(runnable, "SplunkRawTCPInput-OutStream-Serializer");
@@ -41,12 +42,9 @@ public class SplunkRawTCPInput extends SplunkInput {
      * @throws Exception
      */
     public SplunkRawTCPInput(String host, int port) throws Exception {
-
         this.host = host;
         this.port = port;
-
         openStream();
-
     }
 
     /**
@@ -55,8 +53,7 @@ public class SplunkRawTCPInput extends SplunkInput {
     private void openStream() throws Exception {
         streamSocket = new Socket(host, port);
         if (streamSocket.isConnected()) {
-            ostream = streamSocket.getOutputStream();
-            writerOut = new OutputStreamWriter(ostream, "UTF8");
+            writerOut = new OutputStreamWriter(streamSocket.getOutputStream(), "UTF8");
         }
     }
 
@@ -89,25 +86,15 @@ public class SplunkRawTCPInput extends SplunkInput {
                 public void run() {
                     String currentMessage = message;
                     try {
-
                         if (writerOut != null) {
-
-                            //send the message
                             writerOut.write(currentMessage + "\n");
-
-                            //flush the queue
-                            while (queueContainsEvents()) {
-                                String messageOffQueue = dequeue();
-                                currentMessage = messageOffQueue;
-                                writerOut.write(currentMessage + "\n");
-                            }
                             writerOut.flush();
                         }
 
                     } catch (IOException e) {
 
                         //something went wrong , put message on the queue for retry
-                        enqueue(currentMessage);
+                        executor.submit(this);
                         try {
                             closeStream();
                         } catch (Exception e1) {
